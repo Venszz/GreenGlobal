12a42eb015e5d94394bb872e1d9e46a0f844dd2d
DinhHieu <trandinhhieuit@gmail.com>
2024-05-06 10:13:45 +0700

    init commit

---
+++ NewsWave/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewsWave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
